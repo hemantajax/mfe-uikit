a6ec600cb06f716bfead4f762bc8b7ba8d3f855c
Hemant <hemant.ajax@gmail.com>
2025-10-24 07:46:03 +0530

    header cleanup

--- libs/uikit/src/lib/layout/header/header.component.ts
+++ libs/uikit/src/lib/layout/header/header.component.ts
@@ -1,21 +1,16 @@
-import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { CounterService } from '@nxmfe/shared/services';
 
 @Component({
-  selector: 'app-header',
+  selector: 'lib-header',
   imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-  protected counterService = inject(CounterService);
   isMenuCollapsed = true;
-
-  // Use computed to ensure reactivity across MFE boundaries
-  protected counter = this.counterService.counter;
 
   toggleMenu() {
     this.isMenuCollapsed = !this.isMenuCollapsed;
